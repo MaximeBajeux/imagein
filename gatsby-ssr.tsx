6a1b5346d8b2b4b9193c22c7b5adec7a8dd8f4f2
Maxime Bajeux <max.bajeux@gmail.com>
2023-08-14 19:34:26 +0200

    a try

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -5,3 +5,23 @@ import RootElement from "./src/components/rootelement/rootelement";
 export const wrapRootElement = ({ element }) => {
   return <RootElement>{element}</RootElement>;
 };
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="partytown-vanilla-config"
+      dangerouslySetInnerHTML={{
+        __html: `partytown = {
+          resolveUrl={function(url) {
+            if (url.hostname === "connect.facebook.net") {
+              var proxyUrl = new URL('https://image-in.net/');
+              proxyUrl.searchParams.append('url', url.href);
+              return proxyUrl;
+            }
+            return url;
+          },
+        }`,
+      }}
+    />,
+  ]);
+};
