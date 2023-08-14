6febe46a7eb97cf0a34b847598b6e4be23a2bd38
Maxime Bajeux <max.bajeux@gmail.com>
2023-08-14 18:34:42 +0200

    try to fix google tag manager

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,20 +1,27 @@
+import React from "react";
 import "./src/styles/normalize.scss";
 import "./src/styles/themes.scss";
 import "@dotlottie/player-component";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import RootElement from "./src/components/rootelement/rootelement";
+
+export const wrapRootElement = ({ element }) => {
+  return <RootElement>{element}</RootElement>;
+};
+
 export const onRouteUpdate = ({ location }) => {
   if (process.env.NODE_ENV !== "production") {
     return null;
   }
-
   const pagePath = location
     ? location.pathname + location.search + location.hash
     : undefined;
   setTimeout(() => {
-    if (typeof gtag === "function") {
-      gtag("event", "page_view", { page_path: pagePath });
+    if (typeof window.gtag === "function") {
+      window.gtag("event", "page_view", { page_path: pagePath });
     }
   }, 100);
+  return true;
 };
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,42 +1,7 @@
 import React from "react";
-import { Partytown } from "@builder.io/partytown/react";
 
-const ORIGIN = "https://www.googletagmanager.com";
+import RootElement from "./src/components/rootelement/rootelement";
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  if (
-    process.env.NODE_ENV !== "production" &&
-    process.env.NODE_ENV !== "test"
-  ) {
-    return null;
-  }
-
-  const partytownComp = <Partytown key="partytown" forward={["gtag"]} />;
-
-  const googleAnalyticsScript = (
-    <script
-      key="google-analytics"
-      type="text/partytown"
-      src={`${ORIGIN}/gtag/js?id=${process.env.GATSBY_GA_MEASUREMENT_ID}`}
-    />
-  );
-
-  const googleAnalyticsConfig = (
-    <script
-      key="google-analytics-config"
-      type="text/partytown"
-      dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
-        window.gtag = function gtag(){ window.dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.GATSBY_GA_MEASUREMENT_ID}', { send_page_view: false })`,
-      }}
-    />
-  );
-
-  setHeadComponents([
-    partytownComp,
-    googleAnalyticsScript,
-    googleAnalyticsConfig,
-  ]);
+export const wrapRootElement = ({ element }) => {
+  return <RootElement>{element}</RootElement>;
 };
