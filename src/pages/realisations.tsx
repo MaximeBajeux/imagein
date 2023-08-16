9d0f8028a0af3367f239fd8b61978d099faa0a4c
Maxime Bajeux <max.bajeux@gmail.com>
2023-08-16 09:10:29 +0200

    fix

--- src/pages/realisations.tsx
+++ src/pages/realisations.tsx
@@ -41,7 +41,7 @@ const RealisationsPage: React.FC<PageProps<Queries.RealisationListQuery>> = ({
           Nos réalisations
         </h1>
         <p>
-          Nous avons de la chance de travailler avec des clients qui nous font
+          Nous avons la chance de travailler avec des clients qui nous font
           confiance, et c'est ici que l'on vous en parle !
         </p>
       </Herobanner>
